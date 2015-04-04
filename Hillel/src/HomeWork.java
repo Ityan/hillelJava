80e6da226a408970fd2e0f0c7aa802a8f3ee47cb
ITyan <I89tyan@gmail.com>
2015-04-04 21:45:00 +0300

    Home work (lesson 1)

--- Hillel/src/HomeWork.java
+++ Hillel/src/HomeWork.java
@@ -1,6 +1,4 @@
 /**
- * В этой программе демонстрируется нахождение сторон
- * прямоугольника исходя из площади и периметра
  * Created by ITyan on 04.04.2015.
  */
 public class HomeWork {
@@ -12,21 +10,20 @@ public class HomeWork {
     }
 
     /**
-     * Метод решает квадратное уравнение
+     * Method solves the quadratic equation
      * pow(x, 2) - ((perimeter / 2) * x) + area = 0
      *
-     * @param perimeter Периметр прямоугольника
-     * @param area      Площадь прямоугольника
+     * @param perimeter perimeter of a rectangle
+     * @param area      area of a rectangle
      */
     public static void resolve(double perimeter, double area) {
         double width;
         double height;
 
-        //расчитываем дискриминант
+        //counting discriminant
         double d = Math.pow((perimeter / 2), 2) - 4 * area;
 
-        //расчитываем высоту и ширину через квадратное уравнение
-        //исходя из значения дискриминанта
+        //counting the height and width through the quadratic equation
         if (d > 0) {
             width = ((perimeter / 2) + Math.sqrt(d)) / 2;
             height = ((perimeter / 2) - Math.sqrt(d)) / 2;
