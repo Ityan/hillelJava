1a3adf20b1c814fe50bf05154aab81e094fb303d
ITyan <I89tyan@gmail.com>
2015-04-05 12:54:48 +0300

    Home work (lesson 1)

--- Hillel/src/HomeWork.java
+++ Hillel/src/HomeWork.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by ITyan on 04.04.2015.
  */
 public class HomeWork {
     public static void main(String[] args) {
-        double perimeter = 20;
-        double area = 25;
+        double perimeter;
+        double area;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Insert perimeter: ");
+        perimeter = in.nextDouble();
+        System.out.print("Insert area: ");
+        area = in.nextDouble();
 
         resolve(perimeter, area);
     }
