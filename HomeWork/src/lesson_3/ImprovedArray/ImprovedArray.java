39dc6786aaeac46ba3af46c182405628b2aa3071
ITyan <I89tyan@gmail.com>
2015-04-14 20:46:52 +0300

    new

--- HomeWork/src/lesson_3/ImprovedArray/ImprovedArray.java
+++ HomeWork/src/lesson_3/ImprovedArray/ImprovedArray.java
@@ -1,7 +1,71 @@
 package lesson_3.ImprovedArray;
 
+import java.util.Arrays;
+
 /**
- * Created by Igor on 14.04.2015.
+ * @author ITyan 14.04.2015.
  */
 public class ImprovedArray {
+    String[] text = new String[10];
+    int countElement = 0;
+
+    /**
+     * Adds a string to an array
+     *
+     * @param value included string
+     */
+    public void add(String value) {
+        if (text.length == countElement + 1) {
+            text = Arrays.copyOf(text, text.length * 2);
+        }
+        text[countElement] = value;
+        countElement++;
+    }
+
+    /**
+     * Returns an array element
+     *
+     * @param index array index element
+     * @return the item at index
+     */
+    public String get(int index) {
+        if (index > countElement) {
+            return null;
+        } else {
+            return text[index];
+        }
+    }
+
+    /**
+     * @return the number of completed elements
+     */
+    public int size() {
+        return countElement;
+    }
+
+    public boolean equals(Object other) {
+        if (other instanceof ImprovedArray) {
+            ImprovedArray otherArray = (ImprovedArray) other;
+            if (this.text.length != otherArray.text.length) {
+                return false;
+            }
+            for (int i = 0; i < text.length; i++) {
+                if (!this.text[i].equals(otherArray.text[i])) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        String result = "[";
+        for (int i = 0; i < size(); i++) {
+            result += "[" + get(i) + "]";
+        }
+        result += "]";
+        return result;
+    }
 }
