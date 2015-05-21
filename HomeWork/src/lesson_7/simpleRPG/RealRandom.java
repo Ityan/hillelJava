7e4810c32019ec2fae2c4f7697b71f8615612da3
ITyan <I89tyan@gmail.com>
2015-05-21 15:09:50 +0300

    RPG  v 1.0

--- HomeWork/src/lesson_7/simpleRPG/RealRandom.java
+++ HomeWork/src/lesson_7/simpleRPG/RealRandom.java
@@ -6,8 +6,11 @@ import java.util.Random;
  * @author ITyan on 20.05.2015.
  */
 public class RealRandom implements RandomBehavior {
+
+    private Random random = new Random();
+
     @Override
     public double random() {
-        return new Random().nextDouble();
+        return random.nextDouble();
     }
 }
